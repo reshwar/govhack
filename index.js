dd83030134cfc4e0a12edc9128204dce56b8357f
reshwar <eshwar.ravikanti@gmail.com>
2020-08-16 00:02:23 +1000

    index js and html
    initial changes with sign up

---
+++ index.js
@@ -0,0 +1,11 @@
+function onload(){
+    console.log("qq",document.getElementById("logout"));
+    //document.getElementById("logout").style.display = "none";
+
+}
+
+function login(){
+    document.getElementById("logout").style.display = "none";
+}
+
+onload();
